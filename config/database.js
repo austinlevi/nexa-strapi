454f118996077cb2e77182e87c23f5c324ca6ad6
Austin Makachola <upfrontretaile@gmail.com>
2025-07-22 19:23:41 +0300

    Fix SSL config for PostgreSQL on Render

--- config/database.js
+++ config/database.js
@@ -1,14 +1,15 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: env('DATABASE_CLIENT', 'postgres'),
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 });
-
