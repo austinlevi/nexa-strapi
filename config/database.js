045ad7e740e32466960dfcf5e52425481d5e3bf2
Austin Makachola <upfrontretaile@gmail.com>
2025-07-22 20:46:43 +0300

    Use env vars for DB config with SSL for Render

--- config/database.js
+++ config/database.js
@@ -1,16 +1,20 @@
+// path: config/database.js
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'bank'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', '0000'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Optional
       ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
     },
+    debug: false,
   },
 });
 
